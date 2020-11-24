6fb11474d691241a3bcd8644cdfec18307f17d4a
in-amigan <60890561+in-amigan@users.noreply.github.com>
2020-11-24 20:10:42 +0300

    Update config.js

--- Login Page/js/config.js
+++ Login Page/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_HkJ9bco1D', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-2', // e.g. us-east-2
-		clientId: '4hedp5qc8clbc2vk5lo7ssiibm' //is this used anywhere?
+        userPoolId: 'us-east-1_CGcxQYSox'   // 'us-east-1_CGcxQYSox', e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+	clientId: '7jgeqnmobdv87hgjj9aurlpcnn' // '7lf8itfqdek9ktm61sgn1dcq8i'
     },
 };
 
