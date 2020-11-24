67ccb9f9f3c7b59282bcf8cdac68ee36830f0c02
in-amigan <60890561+in-amigan@users.noreply.github.com>
2020-11-24 20:11:45 +0300

    Update config.js

--- Register Page/js/config.js
+++ Register Page/js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_CGcxQYSox'   // 'us-east-1_CGcxQYSox', e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_CGcxQYSox',   // 'us-east-1_CGcxQYSox', e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
 	clientId: '7jgeqnmobdv87hgjj9aurlpcnn' // '7lf8itfqdek9ktm61sgn1dcq8i' 
     },
